9ff7c32a9763c877c4abc6197bda3aac05a36090
Anant <anant02sep@gmail.com>
2022-06-10 12:30:13 +0530

    Test out file

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ interface AppProps {}
 
 function App({}: AppProps) {
   // Create the count state.
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(-10);
   // Create the counter (+1 every second).
   useEffect(() => {
     const timer = setTimeout(() => setCount(count + 1), 1000);
