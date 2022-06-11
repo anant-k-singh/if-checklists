4643c7f2fa1a2d4e215ee4e7494560861e32eab6
Anant <anant02sep@gmail.com>
2022-06-11 14:15:05 +0530

    rename folder file

--- docs/dist/index.js
+++ docs/dist/index.js
@@ -1,8 +1,8 @@
-import * as __SNOWPACK_ENV__ from '../_snowpack/env.js';
+import * as __SNOWPACK_ENV__ from '../snowpack/env.js';
 import.meta.env = __SNOWPACK_ENV__;
 
-import React from "../_snowpack/pkg/react.js";
-import ReactDOM from "../_snowpack/pkg/react-dom.js";
+import React from "../snowpack/pkg/react.js";
+import ReactDOM from "../snowpack/pkg/react-dom.js";
 import App from "./App.js";
 import "./index.css.proxy.js";
 ReactDOM.render(/* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(App, null)), document.getElementById("root"));
