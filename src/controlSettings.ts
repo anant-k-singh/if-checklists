e4c24c5fbd8948960d0f413426ccfec0d69a7b76
Anant <anant02sep@gmail.com>
2022-07-09 22:12:33 +0530

    Initialize checklists

---
+++ src/controlSettings.ts
@@ -0,0 +1,68 @@
+export enum MasterSwitch {
+  ON = 'Master Switch - ON',
+  OFF = 'Master Switch - OFF',
+}
+
+export enum ParkingBrakes {
+  SET = 'Parking Brakes - SET',
+  RELEASED = 'Parking Brakes - RELEASED',
+}
+
+export enum Throttle {
+  IDLE = 'Throttle - IDLE',
+}
+
+export enum Spoilers {
+  OFF = 'Spoilers - OFF',
+  ARMED = 'Spoilers - ARMED',
+}
+
+export enum Flaps {
+  RETRACTED = 'Flaps - RETRACTED',
+}
+
+export enum NoSmoking {
+  ON = 'No Smoking - ON',
+  OFF = 'No Smoking - OFF',
+}
+
+export enum FlightPlan {
+  FILED = 'Flight Plan - FILED',
+}
+
+export enum FuelQuantity {
+  CHECK = 'Fuel Quantity - CHECK',
+}
+
+export enum ATIS {
+  CHECK = 'ATIS - CHECK',
+}
+
+export enum SeatBelts {
+  ON = 'Seat Belts - ON',
+  OFF = 'Seat Belts - OFF',
+}
+
+export enum NavLights {
+  ON = 'Nav Lights - ON',
+  OFF = 'Nav Lights - OFF',
+}
+
+export enum BeaconLights {
+  ON = 'Beacon Lights - ON',
+  OFF = 'Beacon Lights - OFF',
+}
+
+export enum StrobeLights {
+  ON = 'Strobe Lights - ON',
+  OFF = 'Strobe Lights - OFF',
+}
+
+export enum LandingLights {
+  ON = 'Landing Lights - ON',
+  OFF = 'Landing Lights - OFF',
+}
+
+export enum PushbackClearance {
+  RECEIVED = 'Pushback Clearance - RECEIVED',
+}
